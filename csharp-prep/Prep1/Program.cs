fe38197ed606cb0df500646e2960882ab5a4b401
JESUS RAMON <ram19039>
2025-01-18 07:24:50 +0100

    Added a second comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,8 @@ class Program
         // Ask for user first name
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
-
+        
+        // Ask for the user last name
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
         
