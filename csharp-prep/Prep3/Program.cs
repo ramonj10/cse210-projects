6ce366efaeee42b4d0178a2821a829e87ac43cd6
JESUS RAMON <ram19039>
2025-01-20 16:48:39 +0100

    magicNumber with tries counting

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Get a random number between 1 and 100
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+         
+        // Loop tries with a do while as it will always take place at least once
+        int guess;
+        int counter = 0;
+
+            do
+            {
+                counter += 1;
+                
+                Console.Write("What is your guess number? ");
+                string userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+            
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                    Console.WriteLine($"You guessed it! just with {counter} tries. ");
+            } while (guess != magicNumber);      
+        }
     }
-}
