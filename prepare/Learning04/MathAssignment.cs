8744fbafe96fa09349a3ae05bda61101f7678c57
JESUS RAMON <ram19039>
2025-02-18 08:29:44 +0100

    Week07 Assigmnet

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,17 @@
+public class MathAssignmnet :Assignment
+{
+    private string _textBookSection;
+    private string _problems;
+
+    public MathAssignmnet(string studentName, string topic, 
+                    string textBookSection, string problems) : base(studentName, topic)
+    {
+        _textBookSection = textBookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textBookSection} Problems {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Jesús", "Programming with classes" );
+        MathAssignmnet mathAssignmnet1 = new MathAssignmnet("Manu", "Welding", "7.3", "8-19");
+        WrittingAssignment writtingAssignment1 = new WrittingAssignment("Mary Waters",
+                                "European History", "The causes of World War II");
+  
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(mathAssignmnet1.GetSummary());
+        Console.WriteLine(mathAssignmnet1.GetHomeworkList());
+        Console.WriteLine(writtingAssignment1.GetSummary());
+        Console.WriteLine(writtingAssignment1.GetWrittingInfo());        
     }
 }
